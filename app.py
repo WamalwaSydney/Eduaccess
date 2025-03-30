6176c47646e02208aa6a522026d9d62efa9c5f4b
Eloi Iradukunda <e.iradukund2@alustudent.com>
2025-03-30 15:19:02 +0200

    Added Login Function

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
+        
 users = []
 
 def register_user():
@@ -10,3 +10,14 @@ def register_user():
     password = input("Enter password: ")
     users.append(User(username, password))
     print("Registration successful!")
+
+def login_user():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    
+    for user in users:
+        if user.username == username and user.password == password:
+            print("Login successful!")
+            return True
+    print("Invalid username or password.")
+    return False
